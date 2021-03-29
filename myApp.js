8f600f4175f7da961a6a33ff4a0074e584ac2db5
Frozies <52686922+Frozies@users.noreply.github.com>
2021-03-28 22:35:46 -0400

    favorite foods query chain sort fix

--- myApp.js
+++ myApp.js
@@ -111,7 +111,7 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  Person.find({favoriteFoods: foodToSearch}).sort(name).limit(2).select({age: 0}).exec((err,doc) => {
+  Person.find({favoriteFoods: foodToSearch}).sort({name: 1}).limit(2).select({age: 0}).exec((err,doc) => {
     if(err) return console.error(err);
     done(null, doc);
   })

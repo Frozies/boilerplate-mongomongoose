2b1cd27bcdce83c7aaabfc15bb1a7bc18381c2f6
Frozies <52686922+Frozies@users.noreply.github.com>
2021-03-28 22:20:14 -0400

    removebyid

--- myApp.js
+++ myApp.js
@@ -93,7 +93,10 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, doc) => {
+    if (err) return console.error(err);
+    done(null, doc);
+  })
 };
 
 const removeManyPeople = (done) => {
